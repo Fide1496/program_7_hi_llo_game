ec39fd4ec2a582b4ed9dd62220152dac4847857b
faithie <faith@toyridge.com>
2025-03-05 09:24:17 -0600

    reworked program flow

--- main.c
+++ main.c
@@ -1,178 +1,231 @@
-#include <signal.h>
-#include <unistd.h>
-#include <stdlib.h>
 #include <stdio.h>
-#include <errno.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include <sys/wait.h>
-#include <fcntl.h>
+#include <errno.h>
+#include <signal.h>
+#include <sys/time.h>
 #include <time.h>
+#include <string.h>
+#include <fcntl.h>
+
+#define GAMES 10
+
+pid_t child_one_pid, child_two_pid;
 
 static int player_one_min=0;
 static int player_one_max=100;
 static int player_two_min=0;
 static int player_two_max=100;
 
-int checkError(int val, const char *msg) {
-    if (val == -1) {
-        perror(msg);
-        exit(EXIT_FAILURE);
+int sig_usr_1 = 0;
+int sig_usr_2 = 0;
+
+int player_one_score = 0;
+int player_two_score = 0;
+
+pid_t pid[3];
+
+
+// Parent signal handler
+void sigHandlerP(int sig){
+    if (sig == SIGUSR1){
+        sig_usr_1 = 1;
     }
-    return val;
+    if(sig == SIGUSR2){
+        sig_usr_2 = 1;
+    }
+    else if (sig == SIGCHLD){
+        wait(NULL);
+    }
+    else if (sig ==SIGINT){
+        kill(child_one_pid, SIGTERM);
+        kill(child_two_pid, SIGTERM);
+        exit(EXIT_SUCCESS);
+    }
 }
 
-void signalHandler(int sig){
-    char response;
+// Child signal handler
+void sigHandlerC(int sig){
+    if (sig == SIGUSR1){
+        sig_usr_1 = 1;
+    }
+    if(sig == SIGUSR2){
+        sig_usr_2 = 1;
+    }
+    else if (sig == SIGINT){
+        sig_usr_1 = 0;
+        sig_usr_2 = 0;
+    }
+    else if (sig == SIGTERM)
+    {
+        exit(EXIT_SUCCESS);
+    }
+}
 
-    if(sig == SIGINT){
-        fflush(stdin);
-        printf("\nAre you sure you want to exit (Y/n)? \n");
-        scanf(" %c", &response);
+// Player one: uses average of min and max as guess
+void player_one(){
 
-        if (response == 'Y' || response == 'y') {
-            // if (pPid>0){
-            //     kill(pPid, SIGTERM);
-            // }
-            exit(EXIT_SUCCESS);
+    signal(SIGUSR1, sigHandlerC);
+    signal(SIGUSR2, sigHandlerC);
+    signal(SIGINT, sigHandlerC);
+    signal(SIGTERM, sigHandlerC);
+
+    int min = 0;
+    int max = 101;
+    int guess;
+
+    while(1){
+        pause();
+
+        min = 0;
+        max = 0;
+
+        while(1){
+            guess = (min + max) /2;
+            int p1_guesses = checkError(open("player1_guesses.txt", O_WRONLY | O_CREAT | O_TRUNC, 0644), "Open player 1 guess file");
+
+            write(p1_guesses, guess, sizeof(int));
+            close(p1_guesses);
+
+            if(sig_usr_1) min = guess;
+            else if (sig_usr_2) max = guess;
+            else break;
+        }
+    }
+}
+
+// Player two: uses random number between min and max as guess
+void player_two(){
+
+    signal(SIGUSR1, sigHandlerC);
+    signal(SIGUSR2, sigHandlerC);
+    signal(SIGINT, sigHandlerC);
+    signal(SIGTERM, sigHandlerC);
+
+    srand(1);
+
+    int min = 0;
+    int max = 101;
+    int guess;
+
+    while(1){
+        pause();
+
+        min = 0;
+        max = 101;
+
+        while(1){
+            //Validate this guessing structure
+            guess = min +rand() % (max-min);
+
+            int p2_guesses = checkError(open("player2_guesses.txt", O_WRONLY | O_CREAT | O_TRUNC, 0644), "Open player 1 guess file");
+
+            write(p2_guesses, guess, sizeof(int));
+            close(p2_guesses);
+
+            if(sig_usr_1) min = guess;
+            else if (sig_usr_2) max = guess;
+            else break;
+
+        }
+    }
+}
+
+void referee(){
+    signal(SIGUSR1, sigHandlerP);
+    signal(SIGUSR2, sigHandlerP);
+    signal(SIGCHLD, sigHandlerP);
+    signal(SIGINT, sigHandlerP);
+
+    sleep(5);
+    kill(child_one_pid, SIGUSR1);
+    kill(child_two_pid, SIGUSR2);
+
+    for (int i = 1; i <= GAMES; i++){
+
+        // Why the heck do I need these????
+        sig_usr_1 = 0;
+        sig_usr_2 = 0;
+
+        // Wait until start signals are both recieved
+        while(!(sig_usr_1 && sig_usr_2)){
+            pause();
         }
 
-        // while(1)
-        // {
-        //     pid = waitpid(0,NULL,0);
-        //     if(pid == -1){
-        //         if(errno ==ECHILD) exit(EXIT_FAILURE);
-        //         if(errno == EINTR) continue;
-        //         perror("wait");
-        //         exit(EXIT_FAILURE);
-        //     }
-    
-        // }
+        int correct_answer = (rand()% 100+1);
+        printf("Game #%d: Correct asnwer is %d\n", i, correct_answer);
+
+        while(1){
+            sig_usr_1 = 0;
+            sig_usr_2 = 0;
+
+            while(!(sig_usr_1 && sig_usr_2)){
+                pause();
+            }
+
+            int p1_guess, p2_guess;
+
+            int p1_guess_file = checkError(open("player_one_guesses.txt", O_RDONLY), "Open player 1 guess file");
+            int p2_guess_file = checkError(open("player_two_guesses.txt", O_RDONLY), "Open player 2 guess file");
+
+            fscanf(p1_guess_file, "%d", &p1_guess);
+            fscanf(p2_guess_file, "%d", &p2_guess);
+
+            int test_guess = read(p1_guess_file)
+
+            close(p1_guess_file);
+            close(p2_guess_file);
+
+            printf("Player 1 guessed: %d, Player 2 guessed: %d\n",p1_guess, p2_guess);
+
+            if(p1_guess == correct_answer){
+                printf("Player 1 wins!\n");
+                player_one_score++;
+                break;
+            }
+            if(p2_guess==correct_answer){
+                printf("Player 2 wins!\n");
+                player_two_score++;
+                break;
+            }
+
+            if(p1_guess < correct_answer)kill(child_one_pid, SIGUSR1);
+            else kill(child_two_pid, SIGUSR2);
+
+            if(p2_guess < correct_answer)kill(child_two_pid, SIGUSR1);
+            else kill(child_two_pid, SIGUSR2);
+
+            kill(child_one_pid, SIGINT);
+            kill(child_two_pid, SIGINT);
+        }
+
+
     }
-    if (sig == SIGCHLD) {
-        printf("Child has exited\n");
-        while(waitpid(-1, NULL, WNOHANG) > 0);
-        exit(EXIT_SUCCESS);
-    }
-    if (sig == SIGUSR1) {
-        printf("Warning! Roll outside of bounds\n");
-    }
-    if (sig == SIGUSR2) {
-        printf("Warning! Pitch outside of bounds\n");
-    }
-    if(sig == SIGTERM){
-        printf("Parent has died, child terminating...\n");
-        exit(EXIT_SUCCESS);
-    }
-}
 
-int player_one_guess(){
+    printf("Final Results: \n");
+    printf("Player 1 score: %d", player_one_score);
+    printf("Player 2 score: %d", player_two_score);
 
-    // Implement guessing strategy
-    int guess = (player_one_max - player_one_min)/2;
-
-    return guess;
-}
-
-int player_two_guess(){
-
-    // Implement guessing strategy
-    int guess = rand() % (player_two_max - player_two_min + 1)
-    + player_two_min;
-
-    return guess;
+    kill(child_one_pid, SIGTERM);
+    kill(child_two_pid, SIGTERM);
 }
 
 int main(){
 
-    pid_t childPID;
-    sigset_t blockSet, oldSet;
-    pid_t child[2];
-    pid_t pid;
-    int i = 0;
-    
-
-    sigemptyset(&blockSet);
-    sigaddset(&blockSet, SIGINT);
-
-    struct sigaction sa;
-    sigemptyset(&sa.sa_mask);
-    sa.sa_handler = signalHandler;
-    sa.sa_flags = 0;
-
-    // Register signals in parent
-    sigaction(SIGCHLD, &sa, NULL);
-    sigaction(SIGUSR1, &sa, NULL);
-    sigaction(SIGUSR2, &sa, NULL);
-
-
-    for(int i; i< 10; i++){
-        printf("Game $%d\n", i+1);
-
-        int answer = rand(); 
-
-        while(1){
-            pid_t player_one = fork();
-            pid_t player_two = fork();
-
-            int p1_guess = player_one_guess();
-            int p2_guess = player_two_guess();
-
-
-
-        }
+    child_one_pid = fork();
+    if (child_one_pid == 0){
+        player_one();
+        exit(EXIT_SUCCESS);
     }
-    
-    for(i = 0; i<10; i++){
-        printf("Game #%d \n", i+1);
-        
-        int x;
-        for(x = 0; x < 2; x++){
 
-
-
-            switch(childPID = fork())
-            {
-            case -1:
-                perror("fork\n");
-                exit(EXIT_FAILURE);
-            case 0:
-    
-                struct sigaction sa_child;
-                sigemptyset(&sa_child.sa_mask);
-                sa_child.sa_handler = signalHandler;
-                sa_child.sa_flags = 0;
-                
-                // Handle SIGTERM in child
-                sigaction(SIGTERM, &sa_child, NULL);
-                
-                int player_one_guess = player_one();
-                
-                printf("Child process done. Exiting...\n");
-                exit(EXIT_SUCCESS);
-    
-            default:
-    
-                // Handles signals from the child to the parent
-                struct sigaction sa;
-                sigemptyset(&sa.sa_mask);
-                sa.sa_handler = signalHandler;
-                sa.sa_flags = 0;
-    
-                // Register signals in parent
-                sigaction(SIGINT, &sa, NULL);
-                sigaction(SIGCHLD, &sa, NULL);
-                sigaction(SIGUSR1, &sa, NULL);
-                sigaction(SIGUSR2, &sa, NULL);
-                
-                sigprocmask(SIG_SETMASK, &oldSet, NULL);
-    
-                while(1) {
-                    pause();  
-                }
-            }
-        }
-        // exit(EXIT_SUCCESS);
-
+    child_two_pid = fork();
+    if(child_two_pid == 0){
+        player_two();
+        exit(EXIT_SUCCESS);
     }
+
+    referee();
 
     exit(EXIT_SUCCESS);
 }
--- test.c
+++ test.c
@@ -1,108 +1,194 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <sys/wait.h>
-#include <time.h>
 #include <signal.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <time.h>
 
-#define MAX_NUM 100
-#define MIN_NUM 1
+#define MAX_GAMES 10
 
-void play_game(int read_fd, int write_fd) {
-    int guess, response;
+int child1_pid, child2_pid;
+int received_sigusr1 = 0, received_sigusr2 = 0;
+int player1_score = 0, player2_score = 0;
 
-    srand(getpid()); // Unique seed for each process
+// Signal handler for the parent
+void parent_signal_handler(int signo) {
+    if (signo == SIGUSR1) {
+        received_sigusr1 = 1;
+    } else if (signo == SIGUSR2) {
+        received_sigusr2 = 1;
+    } else if (signo == SIGCHLD) {
+        wait(NULL); // Reap the child process
+    } else if (signo == SIGINT) {
+        kill(child1_pid, SIGTERM);
+        kill(child2_pid, SIGTERM);
+        exit(0);
+    }
+}
 
+// Signal handler for children
+void child_signal_handler(int signo) {
+    if (signo == SIGUSR1) {
+        received_sigusr1 = 1;
+    } else if (signo == SIGUSR2) {
+        received_sigusr2 = 1;
+    } else if (signo == SIGINT) {
+        received_sigusr1 = 0;
+        received_sigusr2 = 0;
+    } else if (signo == SIGTERM) {
+        exit(0);
+    }
+}
+
+// Function for Player 1 (Binary Search Guessing)
+void player1() {
+    signal(SIGUSR1, child_signal_handler);
+    signal(SIGUSR2, child_signal_handler);
+    signal(SIGINT, child_signal_handler);
+    signal(SIGTERM, child_signal_handler);
+
+    int min = 0, max = 101, guess;
     while (1) {
-        // Generate a guess
-        guess = (rand() % (MAX_NUM - MIN_NUM + 1)) + MIN_NUM;
-        printf("Player (PID %d) guesses: %d\n", getpid(), guess);
+        pause(); // Wait for parent signal
 
-        // Send guess to referee
-        write(write_fd, &guess, sizeof(int));
+        min = 0;
+        max = 101;
+        while (1) {
+            guess = (min + max) / 2;
+            FILE *fp = fopen("player1_guess.txt", "w");
+            fprintf(fp, "%d\n", guess);
+            fclose(fp);
 
-        // Read referee's response
-        read(read_fd, &response, sizeof(int));
+            sleep(1);
+            kill(getppid(), SIGUSR1);
+            pause(); // Wait for parent's response
 
-        if (response == 0) {
-            printf("Player (PID %d) guessed correctly!\n", getpid());
-            exit(EXIT_SUCCESS);
-        } else if (response == -1) {
-            printf("Player (PID %d): Guess was too low.\n", getpid());
-        } else {
-            printf("Player (PID %d): Guess was too high.\n", getpid());
+            if (received_sigusr1) min = guess;
+            else if (received_sigusr2) max = guess;
+            else break;
         }
     }
 }
 
-int main() {
-    int referee_to_p1[2], p1_to_referee[2];
-    int referee_to_p2[2], p2_to_referee[2];
+// Function for Player 2 (Random Guessing)
+void player2() {
+    signal(SIGUSR1, child_signal_handler);
+    signal(SIGUSR2, child_signal_handler);
+    signal(SIGINT, child_signal_handler);
+    signal(SIGTERM, child_signal_handler);
 
-    pipe(referee_to_p1); pipe(p1_to_referee);
-    pipe(referee_to_p2); pipe(p2_to_referee);
+    srand(getpid());
 
-    pid_t pid1 = fork();
-    if (pid1 == 0) { // Player 1 process
-        close(referee_to_p2[0]); close(referee_to_p2[1]);
-        close(p2_to_referee[0]); close(p2_to_referee[1]);
-
-        play_game(p1_to_referee[0], referee_to_p1[1]);
-    }
-
-    pid_t pid2 = fork();
-    if (pid2 == 0) { // Player 2 process
-        close(referee_to_p1[0]); close(referee_to_p1[1]);
-        close(p1_to_referee[0]); close(p1_to_referee[1]);
-
-        play_game(p2_to_referee[0], referee_to_p2[1]);
-    }
-
-    // Referee process
-    close(referee_to_p1[1]); close(p1_to_referee[0]);
-    close(referee_to_p2[1]); close(p2_to_referee[0]);
-
-    srand(time(NULL)); // Seed for random number
-    int correct_number = (rand() % (MAX_NUM - MIN_NUM + 1)) + MIN_NUM;
-    printf("Referee: The correct number is %d (hidden from players)\n", correct_number);
-
-    int p1_guess, p2_guess, response;
-
+    int min = 0, max = 101, guess;
     while (1) {
-        // Read guesses from players
-        read(p1_to_referee[1], &p1_guess, sizeof(int));
-        read(p2_to_referee[1], &p2_guess, sizeof(int));
+        pause(); // Wait for parent signal
 
-        // Check Player 1's guess
-        if (p1_guess == correct_number) {
-            response = 0;
-            write(referee_to_p1[0], &response, sizeof(int));
-            kill(pid2, SIGTERM); // Stop Player 2
-            break;
-        } else if (p1_guess < correct_number) {
-            response = -1;
-        } else {
-            response = 1;
-        }
-        write(referee_to_p1[0], &response, sizeof(int));
+        min = 0;
+        max = 101;
+        while (1) {
+            guess = min + rand() % (max - min);
+            FILE *fp = fopen("player2_guess.txt", "w");
+            fprintf(fp, "%d\n", guess);
+            fclose(fp);
 
-        // Check Player 2's guess
-        if (p2_guess == correct_number) {
-            response = 0;
-            write(referee_to_p2[0], &response, sizeof(int));
-            kill(pid1, SIGTERM); // Stop Player 1
-            break;
-        } else if (p2_guess < correct_number) {
-            response = -1;
-        } else {
-            response = 1;
+            sleep(1);
+            kill(getppid(), SIGUSR2);
+            pause(); // Wait for parent's response
+
+            if (received_sigusr1) min = guess;
+            else if (received_sigusr2) max = guess;
+            else break;
         }
-        write(referee_to_p2[0], &response, sizeof(int));
+    }
+}
+
+// Function for the Parent (Referee)
+void referee() {
+    signal(SIGUSR1, parent_signal_handler);
+    signal(SIGUSR2, parent_signal_handler);
+    signal(SIGCHLD, parent_signal_handler);
+    signal(SIGINT, parent_signal_handler);
+
+    sleep(5); // Initial delay before starting the game
+    kill(child1_pid, SIGUSR1);
+    kill(child2_pid, SIGUSR2);
+
+    for (int game_round = 1; game_round <= MAX_GAMES; game_round++) {
+        received_sigusr1 = received_sigusr2 = 0;
+        
+        while (!(received_sigusr1 && received_sigusr2)) {
+            pause(); // Wait for both players to be ready
+        }
+
+        int target = (rand() % 100) + 1; // Random target number
+        printf("\nGame %d: Target number is %d\n", game_round, target);
+
+        while (1) {
+            received_sigusr1 = received_sigusr2 = 0;
+
+            while (!(received_sigusr1 && received_sigusr2)) {
+                pause(); // Wait for guesses
+            }
+
+            // Read guesses from files
+            int guess1, guess2;
+            FILE *fp1 = fopen("player1_guess.txt", "r");
+            FILE *fp2 = fopen("player2_guess.txt", "r");
+            fscanf(fp1, "%d", &guess1);
+            fscanf(fp2, "%d", &guess2);
+            fclose(fp1);
+            fclose(fp2);
+
+            printf("Player 1 guessed: %d, Player 2 guessed: %d\n", guess1, guess2);
+
+            // Check if either player wins
+            if (guess1 == target) {
+                printf("Player 1 wins!\n");
+                player1_score++;
+                break;
+            }
+            if (guess2 == target) {
+                printf("Player 2 wins!\n");
+                player2_score++;
+                break;
+            }
+
+            // Send feedback signals
+            if (guess1 < target) kill(child1_pid, SIGUSR1);
+            else kill(child1_pid, SIGUSR2);
+
+            if (guess2 < target) kill(child2_pid, SIGUSR1);
+            else kill(child2_pid, SIGUSR2);
+
+            kill(child1_pid, SIGINT);
+            kill(child2_pid, SIGINT);
+        }
     }
 
-    printf("Game Over! The correct number was %d.\n", correct_number);
-    wait(NULL); // Wait for child processes to finish
-    wait(NULL);
+    printf("\nFinal Results:\n");
+    printf("Player 1 Score: %d\n", player1_score);
+    printf("Player 2 Score: %d\n", player2_score);
 
+    kill(child1_pid, SIGTERM);
+    kill(child2_pid, SIGTERM);
+}
+
+int main() {
+    srand(time(NULL)); // Seed random number generator
+
+    child1_pid = fork();
+    if (child1_pid == 0) {
+        player1();
+        exit(0);
+    }
+
+    child2_pid = fork();
+    if (child2_pid == 0) {
+        player2();
+        exit(0);
+    }
+
+    referee();
     return 0;
 }
